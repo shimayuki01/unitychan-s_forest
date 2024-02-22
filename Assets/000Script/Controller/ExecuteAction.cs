e7fe33cc19343a2b724dfaf58dd6101daea950dd
SomaKamikubo <e195708@ie.u-ryukyu.ac.jp>
2024-02-22 23:06:59 +0900

    バッグ画面で画像が表示できるように

--- Assets/000Script/Controller/ExecuteAction.cs
+++ Assets/000Script/Controller/ExecuteAction.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
 
 public class ExecuteAction : MonoBehaviour
 {
@@ -12,6 +15,11 @@ public class ExecuteAction : MonoBehaviour
     [SerializeField] BagContensRenderer _bagContensRenderer;
 
     public GameObject _panelInstance;
+    private AsyncOperationHandle<Sprite> handle;
+    void Start()
+    {
+        handle = Addressables.LoadAssetAsync<Sprite>("Assets/050image/food_curryruce.png");
+    }
 
 
     public void Cook(string cookItem_id)
@@ -36,8 +44,15 @@ public class ExecuteAction : MonoBehaviour
         _baseMenuPanel.CloseButtonClicked();
     }
 
+
     public void openBag()
     {
+
+
+
+
+
+
         foreach (var i in _player.getBagSummary())
         {
             Debug.Log(i.Key + " " + i.Value);
@@ -48,7 +63,14 @@ public class ExecuteAction : MonoBehaviour
         _baseMenuPanel.InstiatePanel(_bagPanel);
         _bagContensRenderer.DisplayItems(_baseMenuPanel._panelInstance, bagSummary);
 
+        // ÉçÅ[ÉhäÆóπÇë“ã@Ç∑ÇÈ
+        GameObject itemPanelParent = _baseMenuPanel._panelInstance.transform.Find("ItemPanel").gameObject;
+        int idx = 0;
 
+        GameObject itemPanel = itemPanelParent.transform.GetChild(idx).gameObject;
+        Debug.Log(itemPanel.transform.Find("Image").gameObject);
+        Image panelText = itemPanel.transform.Find("Image").gameObject.GetComponent<Image>();
+        panelText.sprite = Instantiate(handle.Result);
 
 
     }
--- Assets/000Script/UI/BagContensRenderer.cs
+++ Assets/000Script/UI/BagContensRenderer.cs
@@ -7,7 +7,7 @@ public class BagContensRenderer : MonoBehaviour
 {
     public void DisplayItems(GameObject bagMenuePanel, Dictionary<string, int> bagSummary)
     {
-        //GameObject temp = bagMenuePanel.transform.Find("BagPanel").gameObject;
+
         GameObject itemPanelParent = bagMenuePanel.transform.Find("ItemPanel").gameObject;
         int idx = 0;
         foreach (var item in bagSummary)
@@ -15,7 +15,6 @@ public class BagContensRenderer : MonoBehaviour
             GameObject itemPanel = itemPanelParent.transform.GetChild(idx).gameObject;
             Text panelText = itemPanel.GetComponentInChildren<Text>();
             panelText.text = item.Value.ToString();
-            //Debug.Log("=============== UIŠm”F" + item.Value.ToString());
             idx++;
 
         }
